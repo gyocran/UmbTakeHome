c093288eb884e2563203c13a8dfb67ea1e1cab8b
gyocran <justblaze.0@gmail.com>
2023-02-06 11:40:21 +0000

    Add project files.

---
+++ UmbTakeHome/Pages/Landing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UmbTakeHome.Pages
+{
+    public class LandingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
